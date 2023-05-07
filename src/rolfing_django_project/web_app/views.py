1aa1815a901841225caa7eb073f46f80e30f1286
Andrey Smekhov <myfearismyblue@gmail.com>
2023-05-07 16:58:50 +0300

    Make overview to work with orm

--- src/rolfing_django_project/web_app/views.py
+++ src/rolfing_django_project/web_app/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 
+from .models import EventModel
+
 
 def index(request):
     return render(request, template_name='web_app/about.html')
 
 
 def overview(request):
-    return render(request, template_name='web_app/overview.html')
+    events = EventModel.objects.all()
+    context = {'events': events,
+               }
+    return render(request, template_name='web_app/overview.html', context=context)
 
 # Create your views here.
