428bfb82c44f9cf3a3bdeb5e3fa6cffce5956c24
Andrey Smekhov <myfearismyblue@gmail.com>
2023-05-30 19:37:38 +0300

    Fix bugs with templates and models after django-modeltraslation work

--- src/rolfing_django_project/web_app/models.py
+++ src/rolfing_django_project/web_app/models.py
@@ -1,6 +1,3 @@
-from typing import Optional
-
-from django.contrib.auth.models import User
 from django.db import models
 from django.urls import reverse
 from django.utils.text import slugify
@@ -9,6 +6,7 @@ from django.utils.translation import gettext_lazy as _
 
 
 class TeacherModel(models.Model):
+    id = models.IntegerField(primary_key=True)
     first_name = models.CharField(max_length=64, null=False, blank=False)
     second_name = models.CharField(max_length=64, null=False, blank=False)
     # photo = models.ImageField()     # FIXME: consider URLField here
@@ -22,6 +20,7 @@ class TeacherModel(models.Model):
 
 
 class TopicModel(models.Model):
+    id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=128, null=False, blank=False)
 
     class Meta:
@@ -33,6 +32,7 @@ class TopicModel(models.Model):
 
 
 class Topic_Module(models.Model):
+    id = models.IntegerField(primary_key=True)
     topic = models.ForeignKey(TopicModel, on_delete=models.CASCADE, related_name='modules')
     module = models.CharField(max_length=64, null=True, blank=True)
 
@@ -44,6 +44,7 @@ class Topic_Module(models.Model):
 
 
 class EventModel(models.Model):
+    id = models.IntegerField(primary_key=True)
     start_date = models.DateField()
     end_date = models.DateField()
     country = models.ForeignKey('cities_light.Country', on_delete=models.SET_NULL, null=True, blank=True)
@@ -60,16 +61,17 @@ class EventModel(models.Model):
 
 
 class RegionalAssociationModel(models.Model):
+    id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=128, blank=False, null=False, unique=True)
     slug = models.SlugField(max_length=128, blank=True, null=True, unique=True, default=None)
     address = models.CharField(max_length=512, blank=False, null=False)
-    person = models.CharField(max_length=256, blank=False, null=False)
+    person = models.CharField(max_length=256, blank=True, null=True)
     telephone = models.CharField(max_length=16, blank=True, null=False, default='')
     web_site = models.CharField(max_length=128, blank=True, null=False, default='')
-    e_mail = models.EmailField()
+    e_mail = models.EmailField(blank=True,null=True)
 
     def get_absolute_url(self):
-        return ''.join((reverse('associations'), '/', str(self.slug)))
+        return ''.join((reverse('associations'), '/id', str(self.id)))
 
     def save(self, *args, **kwargs):
         if not self.slug:
