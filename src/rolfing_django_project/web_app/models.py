a3f059b73b5a9984ed18d5bcc8d65b09e3dcfe84
Andrey Smekhov <myfearismyblue@gmail.com>
2023-06-03 16:21:46 +0300

    Restore all migrations to consistent

--- src/rolfing_django_project/web_app/models.py
+++ src/rolfing_django_project/web_app/models.py
@@ -1,7 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.urls import reverse
-from django.utils import timezone
 from django.utils.text import slugify
 from django.utils.translation import gettext_lazy as _
 
